05e4e30b0bf8926ffd38fe51cd00e552962a5204
Kavindi Pathiraja <kavindipathiraja117@gmail.com>
2025-05-18 10:50:33 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/AF2_Frontend",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
